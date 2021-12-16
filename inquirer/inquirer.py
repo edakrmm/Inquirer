8bb3a23e26f39ccd57ae32725413f9ff3b4e4b68
EDA KARAMEMISOGLU <TCEKARAMEMISOGLU@turkcell.entp.tgc>
2021-12-17 00:58:58 +0300

    basic tkinter app is designed

--- inquirer/inquirer.py
+++ inquirer/inquirer.py
@@ -1,4 +1,8 @@
 from cryptography.fernet import Fernet
+from watchdog.events import FileSystemEventHandler
+import os
+from tkinter import *
+
 
 # Generate Key and Store
 
@@ -12,6 +16,7 @@ with open("fernet_key.key", "rb") as f:
     key = f.read()
 
 
+
 #Encrypt File
 with open("Kıbrıs Çağrı Açma.PNG","rb") as f:
     data = f.read()
@@ -20,6 +25,51 @@ with open("Kıbrıs Çağrı Açma.PNG","rb") as f:
     with open("Kıbrıs Çağrı Açma.PNG","wb") as g:
         g.write(encrypted)
 
+# def enter_path():
+#     filePaths.append(E1.get())
+#     print(filePaths)
+
+# def exit_app():
+#     top.destroy
+
+
+
+
+class App(Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.entry = Entry(self)
+        self.entry.focus()
+        self.entry.pack()
+        self.enterPathButton = Button(self, text="Enter", width=10, command=self.enter_path)
+        self.exitButton= Button(self, text="Quit", width=10, command=self.exit_app)
+        self.exitButton.pack(side = BOTTOM)
+        self.enterPathButton.pack(side = BOTTOM)
+        self.filePaths = []
+
+    def enter_path(self):
+        self.filePaths.append(self.entry.get())
+        self.entry.delete(0, 'end')
+        print(self.filePaths)
+
+    def exit_app(self):
+        self.master.destroy()
+
+
+def main():
+    root = Tk()
+    App(root).pack(expand=True, fill='both')
+    root.after(10000, root.destroy)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self,event):
+        print("asd")
 
 #Decrypt File
 with open("Kıbrıs Çağrı Açma.PNG","rb") as f:
@@ -27,5 +77,4 @@ with open("Kıbrıs Çağrı Açma.PNG","rb") as f:
     decrypted = fernet.decrypt(data)
     with open("Kıbrıs Çağrı Açma.PNG","wb") as g:
         g.write(decrypted)
-
 
